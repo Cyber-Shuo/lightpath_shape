d35c2cf48fe50a2dcc5edb54feba772eaf93c0ea
CyberShuo <117975967+Cyber-Shuo@users.noreply.github.com>
2023-05-21 20:29:42 +0800

    Update and rename test1.py to lightpath_shape_V1.0.py

--- lightpath_shape_V1.0.py
+++ lightpath_shape_V1.0.py
@@ -3,7 +3,6 @@ import numpy as np
 import mpl_toolkits.axisartist as axisartist
 from scipy.interpolate import make_interp_spline
 
-Y = 0
 X_s,Y_s = [],[]
 with open('D:\\STUDY_think\\Data\\ALL_Figure\\python_fig\\python_light_shape\\nolens.txt','r') as f:
     lines = f.readlines()
@@ -28,18 +27,16 @@ Y_l = np.array(Y_l)/2
 X_l_smooth = np.linspace(200, 1300, 12800)
 Y_l_smooth = make_interp_spline(X_l, Y_l)(X_l_smooth)
 
-plt.rcParams['font.sans-serif'] = ['simHei']
-plt.label = 0
 plt.plot(X_s, Y_s, color = 'b', label = 'no lens', marker="o", markersize=10)
 plt.plot(X_l, Y_l, color = 'r', label = r'$l_{long}$', marker="x", markersize=10)
 plt.fill_between(X_s, Y_s, Y, color = 'b', alpha = 0.3)
 plt.fill_between(X_l, Y_l, Y, color = 'r', alpha = 0.5)
-plt.plot(X_s_smooth, Y_s_smooth, color = 'black', label = '插值法包络线')
+plt.plot(X_s_smooth, Y_s_smooth, color = 'black', label = 'Interpolation method envelope')
 plt.plot(X_l_smooth, Y_l_smooth, color = 'black')
 
-plt.xlabel('光源到PMT距离/mm')
-plt.ylabel('光斑半径/mm')
+plt.xlabel('Light source to PMT distance/mm')
+plt.ylabel('Spot radius/mm')
 plt.legend()
-plt.title('光路比较')
+plt.title('Optical path comparison')
 plt.show()
 
